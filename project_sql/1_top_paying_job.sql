3238259e2fb1b01e087f4b921298eac770b5f7b6
Nymeria Nenya <urska8izjak@gmail.com>
2025-04-09 20:33:43 +0200

    added junior data analyst position

--- project_sql/1_top_paying_job.sql
+++ project_sql/1_top_paying_job.sql
@@ -16,8 +16,26 @@ FROM
 LEFT JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id
 WHERE 
     job_work_from_home IS true AND 
-    job_title_short = 'Data Scientist' AND
+    job_title_short = 'Data Analyst' AND
     salary_year_avg is NOT null
+ORDER BY 
+    salary_year_avg desc
+LIMIT 10
+
+
+--junior data analyst--
+SELECT
+    job_title,
+    company_dim.name AS company_name,
+    job_country,
+    salary_year_avg
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id
+WHERE 
+    job_work_from_home IS true AND 
+    job_title_short = 'Data Analyst' AND
+    job_title like '%junior%'
 ORDER BY 
     salary_year_avg desc
 LIMIT 10
